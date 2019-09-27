05df462a879b9da6c717ac3d49659e8449d5ddeb
Shiv Alagar <ashivalagar@gmail.com>
2019-09-28 00:52:48 +0800

    dsjbvkj

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,36 +1,79 @@
 import React from 'react';
 import Todos from './Todos';
 import NewTodo from './NewTodo';
+import axios from 'axios';
+
+var fetch = require("node-fetch");
 
 
 class App extends React.Component {
   state = {
-    todos: [
-      { id: 1, content: "Buy some milk" },
-      { id: 2, content: "Play mario kart" }
-    ]
+    todos: ''
   }
 
   completeItem = (id) => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    });
+    var options = {
+      method: 'POST',
+      url: 'http://8bc15796.ngrok.io/remove',
+      headers:
+      {
+        'Postman-Token': '41582a4d-a65f-40d8-ac5d-ca27b473348d',
+        'cache-control': 'no-cache',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: id }),
+      json: true
+    };
 
-    this.setState({
-      todos: todos
-    })
+    fetch(options.url, options).then((res) => res.json()).then((res) => {
+      console.log(res);
+      const todos = this.state.todos.filter(todo => {
+        return todo.id !== res.id;
+      });
+      this.setState({
+        todos: todos
+      })
+    });
   }
 
   addTodo = (todo) => {
-    todo.id = Math.random();
+    todo.id = Math.floor(20 * Math.random());
     // console.log(todo)
-    let todos = [...this.state.todos, todo]                  //Spread operator dumps each induvidual item into the array
-    console.log(todos);
-    this.setState({
-      todos
-    })
+    // let todos = [...this.state.todos, todo]                  //Spread operator dumps each induvidual item into the array
+    // console.log(todos);
+    // this.setState({
+    //   todos
+    // })
+    console.log(todo)
+    var options = {
+      method: 'POST',
+      url: 'http://8bc15796.ngrok.io/create',
+      headers:
+      {
+        'Postman-Token': '41582a4d-a65f-40d8-ac5d-ca27b473348d',
+        'cache-control': 'no-cache',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo),
+      json: true
+    };
+
+    fetch(options.url, options).then((res) => res.json()).then((res) => {
+      console.log(res);
+      const todos = [...this.state.todos, res]
+      this.setState({ todos });
+    });
   }
 
+  componentDidMount = () => {
+    axios.get("http://8bc15796.ngrok.io/all").then((res) => {
+      var data = res.data;
+      console.log(data)
+      this.setState({
+        todos: data
+      })
+    })
+  }
   render() {
     return (
       <div className="todo-app container">
